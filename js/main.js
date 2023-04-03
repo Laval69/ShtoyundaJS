9dfb8f3a032817f3fe644f48a19761de79af3c1e
Laval69 <127093527+Laval69@users.noreply.github.com>
2023-04-03 22:18:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,71 +1,125 @@
-let str = "";
+//ДЗ 5 по Роминому подсчету
 
-for (let i = 10; i <= 20; i++) {
-  str += i;
-  if (i < 20) {
-    str += ", ";
-  }
-}
-console.log(str);
-
-let kva = "";
-for (let i = 10; i <= 20; i++) {
-  kva += i * i;
-  if (i < 20) {
-    kva += ", ";
-  }
-}
-console.log(kva);
-
-
-let proiz = 1;
-
-for (let i = 15; i <= 35; i++) {
-  proiz  *= i;
-}
-console.log(proiz);
-
+const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
 let sum = 0;
+let positiveNum = 0;
 
-for (let i = 1; i <= 500; i++) {
-  sum += i;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0) {
+    positiveNum++;
+  }
+  sum += arr[i];
 }
-const aref = sum / 500;
-console.log(aref);
+// arr.forEach(function(elem) {  
+//     if(elem > 0){
+//       sum += elem
+//       positiveNum++
+//     }
+//   });
+console.log("Сума елементов массива: " + sum);
+console.log("Позитивные елементы " + positiveNum);
 
-let suma = 0;
 
-for (let i = 30; i <= 80; i++) {
-  if (i % 2 === 0) {
-    suma += i;
+let min = arr[0];
+let minIndex = 0;
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] < min) {
+    min = arr[i];
+    minIndex = i;
   }
 }
-console.log(suma);
 
-for (let i = 100; i <= 200; i++) {
-  if (i % 3 === 0) {
-    console.log(i);
+console.log(`Минимальный елемент: ${min}`);
+console.log(`Порядковый номер елемента: ${minIndex}`);
+
+let max = arr[0];
+let maxIndex = 0;
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+    maxIndex = i;
   }
 }
 
-const num = prompt('Введите число'); // замените на любое целое число для проверки
+console.log(`Максимальный елемент: ${max}`);
+console.log(`Порядковый номер елемента: ${maxIndex}`);
 
-if (num < 2) {
-  alert(`${num} - составное число`);
-} else {
-  let isPrime = true;
-  
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      isPrime = false;
-      break;
-    }
+
+let negative = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < 0) {
+    negative++;
   }
-  
-  if (isPrime) {
-    alert(`${num} - простое число`);
+}
+
+console.log(`Количество негативних елементов: ${negative}`);
+
+
+let numOdd = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0 && arr[i] % 2 !== 0) {
+    numOdd++;
+  }
+}
+
+console.log(`Количество непарних позитивних елементов: ${numOdd}`);
+
+let numEven = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0 && arr[i] % 2 === 0) {
+    numEven++;
+  }
+}
+
+console.log(`Количество парних позитивних елементов: ${numEven}`);
+
+
+
+let sumEven = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0 && arr[i] % 2 === 0) {
+    sumEven += arr[i];
+  }
+}
+
+console.log(`Сума парних позитивних елементов: ${sumEven}`);
+
+
+
+let sumOdd = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0 && arr[i] % 2 !== 0) {
+    sumOdd += arr[i];
+  }
+}
+
+console.log(`Сума непарних позитивних елементів: ${sumOdd}`);
+
+let product = 1;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0) {
+    product *= arr[i];
+  }
+}
+
+console.log(`Произведение позитивних елементов: ${product}`);
+
+
+const newArr = arr.map(elem => {
+
+  if (elem !== max) {
+      return 0;
   } else {
-    alert(`${num} - составное число`);
+      return elem;
   }
-}
+});
+console.log(`Обновленный массив ${newArr}`);
